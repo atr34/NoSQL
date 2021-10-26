d64d225c9d9f11b2c59b3e7dbd58606a64455051
atr34 <59709340+atr34@users.noreply.github.com>
2021-10-25 22:20:00 -0400

    All necessary code files added

---
+++ dynamodb.py
@@ -0,0 +1,94 @@
+import boto3
+import csv as csv
+
+def bucket():
+    s3 = boto3.resource(
+        service_name='s3',
+        region_name='us-east-2',
+        aws_access_key_id='AKIAZTW5UUKAECOO7D6X',
+        aws_secret_access_key='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'
+    )
+
+
+    try:
+        s3.create_bucket(Bucket='cs1660.atr34', CreateBucketConfiguration={
+            'LocationConstraint': 'us-east-2'})
+    except Exception as e:
+        print(e)
+
+    #we will make this bucket publicly readable
+    bucket = s3.Bucket("cs1660.atr34")
+    bucket.Acl().put(ACL='public-read')
+
+
+    #Create the dynamodb database
+    dyndb = boto3.resource('dynamodb',
+        region_name='us-east-2',
+        aws_access_key_id='AKIAZTW5UUKALBPIASER',
+        aws_secret_access_key='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'
+    )  
+
+    try:
+        table = dyndb.create_table(
+            TableName='csv_table',
+            KeySchema = 
+            [
+                {
+                    'AttributeName': 'PartitionKey',
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'RowKey',
+                    'KeyType': 'RANGE'
+                }
+            ],
+            AttributeDefinitions = 
+            [
+                {
+                    'AttributeName': 'PartitionKey',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'RowKey',
+                    'AttributeType': 'S'
+                },
+            ],
+            ProvisionedThroughput = 
+            {
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
+        )
+    except Exception as e:
+        print (e)  
+
+        #if there is an exception, the table may already exist. if so...
+        table = dyndb.Table('csv_table')
+
+    #wait for the table to be created
+    table.meta.client.get_waiter('table_exists').wait(TableName='csv_table')
+    print(table.item_count)
+
+    with open('experiments.csv', 'r') as csvfile:
+        csvf = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for item in csvf: 
+            print(item) 
+            if item[4].__contains__('.csv') == True:
+                body = open(item[4], 'rb')            
+            else:
+                continue
+            print('body')
+            s3.Object('datacont-name', item[4]).put(Body=body)
+            md = s3.Object('datacont-name', item[4]).Acl().put(ACL='public-read')
+            url = " https://s3-us-east-2.amazonaws.com/datacont-name/"+item[3]
+            metadata_item = {'PartitionKey': item[0], 'RowKey': item[1],
+                'description' : item[4], 'date' : item[2], 'url':url}
+
+            try:
+                table.put_item(Item=metadata_item)
+            except:
+                print("item may already be there or another failure")
+
+bucket()
+
+print('success')
